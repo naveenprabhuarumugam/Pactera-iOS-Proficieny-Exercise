199d77a52001d358bf240f3bd05f54f7d9bec27d
Naveenprabhu Arumugam <naveenprabhu.a@gmail.com>
2015-08-01 00:03:06 +1000

    Add loading Indicator to Splash screen and align it with Autolayout

--- PacteraApp/PacteraApp/Classes/Controllers/SplashScreenViewController.m
+++ PacteraApp/PacteraApp/Classes/Controllers/SplashScreenViewController.m
@@ -10,6 +10,8 @@
 
 @interface SplashScreenViewController ()
 
+@property (nonatomic,strong) UIActivityIndicatorView *activityIndicatorView;
+
 @end
 
 @implementation SplashScreenViewController
@@ -18,6 +20,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self.view setBackgroundColor:[UIColor greenColor]];
+    
+    // Instantiates the activityindicator View and releasing it after transferrign the ownership
+    UIActivityIndicatorView *activityIndicatorObj= [[UIActivityIndicatorView alloc]init];
+    self.activityIndicatorView =activityIndicatorObj;
+    [activityIndicatorObj release];
+    
+    [self.view addSubview:self.activityIndicatorView];
+    
+    // Set TranslatesAutoresizingMaskIntoConstraints to 'No' for the UIElement to behave as per the Autolayout
+    [self.activityIndicatorView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    // Aligns the activity indicator view with centre X of its superview
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.activityIndicatorView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
+    
+    // Aligns the activity indicator view with centre Y of its superview
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.activityIndicatorView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1 constant:0]];
+    
 }
 
 - (void)didReceiveMemoryWarning {
